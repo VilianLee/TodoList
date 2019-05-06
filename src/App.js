d79f824aa7a0f6d70727100b6b45a99fc0392e0a
vilian <276550763@qq.com>
2019-05-06 11:31:33 +0800

    新建leanCloud文件

--- src/App.js
+++ src/App.js
@@ -12,15 +12,6 @@ import TodoItem from './component/TodoItem/TodoItem'
 
 import * as localStore from './localStore'
 
-let  AV = require('leancloud-storage');
-
-const APP_ID = 'srRRFpIqgAj7JxkRmHgfa1UY-gzGzoHsz';
-const APP_KEY = 'TP7ldeFDxBpqpj111hPnskSc';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
 
 
 
