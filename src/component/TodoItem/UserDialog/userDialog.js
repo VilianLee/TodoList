a8f48e1631ea9146d7ccdaacf6b6962903c8d808
vilian <276550763@qq.com>
2019-05-07 11:49:47 +0800

    使用leanCloud完成注册功能

--- src/component/TodoItem/UserDialog/userDialog.js
+++ src/component/TodoItem/UserDialog/userDialog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {signUp} from '../../../leanCloud'
 
 import './UserDialog.css'
 export default class UserDialog extends Component{
@@ -17,7 +18,17 @@ export default class UserDialog extends Component{
       selected: e.target.value
     })
   }
-  signUp(e){}
+  signUp(e){
+    e.preventDefault()
+    let {username, password} = this.state.formData
+    let success = (user)=>{
+      console.log(user)
+    }
+    let error = (error)=>{
+      console.log(error)
+    }
+    signUp(username, password, success, error)
+  }
   signIn(e){}
   changeFormData(key, e){
     // this.state.formData.username = e.target.value
